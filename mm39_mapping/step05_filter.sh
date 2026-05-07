264efa8418dba1eb1065896abda21cbee350b442
Kristian Ullrich <ullrich@evolbio.mpg.de>
2026-05-07 22:28:17 +0200

    Update step05_filter.sh

--- mm39_mapping/step05_filter.sh
+++ mm39_mapping/step05_filter.sh
@@ -1,11 +1,9 @@
 /opt/bcftools/bcftools-1.21/bcftools filter \
--e "sSUM(FORMAT/AD)<5 | MAX(FORMAT/GQ)<30" -g 3 -G 5 -S \
-GER.1.autosomes.g.vcf.gz \
---threads 12 | /opt/bcftools/bcftools-1.21/bcftools view --threads 12 -m 2 -i 'GT~"1"' -v snps -Oz -o \
+-e "sSUM(FORMAT/AD)<5 | MAX(FORMAT/GQ)<30" -g 3 -G 5 -S . --threads 12 \
+GER.1.autosomes.g.vcf.gz | /opt/bcftools/bcftools-1.21/bcftools view --threads 12 -m 2 -i 'GT~"1"' -v snps -Oz -o \
 GER.1.autosomes.DP5.GQ30.SnpGap3.IndelGap5.vcf.gz
 
 /opt/bcftools/bcftools-1.21/bcftools filter \
--e "sSUM(FORMAT/AD)<5 | MAX(FORMAT/GQ)<30" -g 3 -G 5 -S \
-GER.1.chrX.g.vcf.gz \
---threads 12 | /opt/bcftools/bcftools-1.21/bcftools view --threads 12 -m 2 -i 'GT~"1"' -v snps -Oz -o \
+-e "sSUM(FORMAT/AD)<5 | MAX(FORMAT/GQ)<30" -g 3 -G 5 -S . --threads 12 \
+GER.1.chrX.g.vcf.gz | /opt/bcftools/bcftools-1.21/bcftools view --threads 12 -m 2 -i 'GT~"1"' -v snps -Oz -o \
 GER.1.chrX.DP5.GQ30.SnpGap3.IndelGap5.vcf.gz
